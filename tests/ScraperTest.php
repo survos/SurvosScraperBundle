f35aeabf30ebbca84a89de0620c2aa0c613a4255
tacman <info@survos-ci.com>
2023-05-10 16:42:53 +0000

    prepare release

---
+++ tests/ScraperTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Survos\Scraper\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+use Survos\Scraper\Service\ScraperService;
+use Symfony\Component\HttpClient\MockHttpClient;
+use Symfony\Component\HttpClient\Response\MockResponse;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class ScraperTest extends TestCase
+{
+    public function testScraper()
+    {
+        $dir = '../data/museum-digital';
+        $scraperService = $this->createMockService($dir);
+
+        $this->assertSame($dir, $scraperService->getDir());
+
+        $dir = '../data/museum-digital-cache';
+        $this->assertSame($dir, $scraperService->setDir($dir)->getDir());
+    }
+
+    public function testFetchUrl()
+    {
+        $dir = '../data/museum-digital-cache';
+        $scraperService = $this->createMockService($dir);
+
+        $response = $scraperService->fetchUrl('https://exmaple.com/test');
+        $this->assertSame(json_encode([
+            [
+                'title' => 'Example',
+                'labels' => ['Test label'],
+            ]
+        ]), $response);
+    }
+
+    /**
+     * @param string $dir
+     * @return ScraperService
+     */
+    private function createMockService(string $dir): ScraperService
+    {
+        $mockResponse = new MockResponse(json_encode([
+            'title' => 'Example',
+            'labels' => ['Test label'],
+        ]));
+
+        return new ScraperService(
+            $dir,
+            new MockHttpClient($mockResponse),
+            $this->createMock(CacheInterface::class),
+            $this->createMock(LoggerInterface::class)
+        );
+    }
+}
